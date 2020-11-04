0acb041f32215ac7fa99163f931a116f0da3635a
mad_haosist <Legion-fire@yandex.ru>
2020-11-04 20:23:50 +0300

    дополнил десяткой и сотней Создал класс романской системы счисления

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static boolean roman_flag = false;
+    static boolean roman_flag;
     static int result;
     static String result_roman;
     public static void main(String[] args) throws IOException {
@@ -97,8 +97,9 @@ public class Main {
         int number1 = Integer.parseInt(number_and_operation[0]);
         int number2 = Integer.parseInt(number_and_operation[2]);
         String arithmetic_operation = number_and_operation[1];
+
         if (number1 > 10 || number2 > 10) throw new ArithmeticException("Одно из введенных чисел, или оба числа больше 10");
-        if (number1 < 0 || number2 < 0) throw new ArithmeticException("Одно из введенных чисел, или оба числа меньше единицы");
+       if (number1 < 0 || number2 < 0) throw new ArithmeticException("Одно из введенных чисел, или оба числа меньше единицы");
            result = switch (arithmetic_operation) {
             case "+" -> number1 + number2;
             case "-" -> number1 - number2;
@@ -107,83 +108,6 @@ public class Main {
             default -> throw new ArithmeticException("Введена неверная операция");
         };
          if (!roman_flag) System.out.println(result);
-     else roman_numerals(result); // метод романус
-    }
-
-    public static String roman_ten (int a) {
-        if (roman_flag) {
-            switch (a) {
-                case 0 -> {
-                    result_roman = "";
-                }
-                case 1 -> {
-                    result_roman = "I";
-                }
-                case 2 -> {
-                    result_roman = "II";
-                }
-                case 3 -> {
-                    result_roman = "III";
-                }
-                case 4 -> {
-                    result_roman = "IV";
-                }
-                case 5 -> {
-                    result_roman = "V";
-                }
-                case 6 -> {
-                    result_roman = "VI";
-                }
-                case 7 -> {
-                    result_roman = "VII";
-                }
-                case 8 -> {
-                    result_roman = "VIII";
-                }
-                case 9 -> {
-                    result_roman = "IX";
-                }
-                case 10 -> {
-                    result_roman = "X";
-                }
-            }
-        }
-        return result_roman;
-    }
-
-    static void roman_numerals (int a) {
-        if (roman_flag) {
-            if (result <= 10) {
-                roman_ten(result);
-            } else {
-                result_roman = String.valueOf(result);
-                String result_roman_first;
-                String result_roman_second;
-                char[] ch = result_roman.toCharArray();
-                result_roman_first = String.valueOf(ch[0]);
-                result_roman_second = String.valueOf(ch[1]);
- //               System.out.println(ch.length);
-   //             System.out.println(result_roman_first);
-     //           System.out.println(result_roman_second);
-                if (ch.length  == 3) {
-                    result_roman_first = "10";
-                }
-                roman_ten(Integer.parseInt(result_roman_second));
-                switch (result_roman_first) {
-                    case "1" -> result_roman = "X" + result_roman;
-                    case "2" -> result_roman = "XX" + result_roman;
-                    case "3" -> result_roman = "XXX" + result_roman;
-                    case "4" -> result_roman = "XL" + result_roman;
-                    case "5" -> result_roman = "L" + result_roman;
-                    case "6" -> result_roman = "LX" + result_roman;
-                    case "7" -> result_roman = "LXX" + result_roman;
-                    case "8" -> result_roman = "LXXX" + result_roman;
-                    case "9" -> result_roman = "XC" + result_roman;
-                    case "10" -> result_roman = "C";
-                }
-
-            }
-            System.out.println(result_roman);
-        }
+     else Roman.roman_numerals(result); // метод романус
     }
 }
