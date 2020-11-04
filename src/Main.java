fc946ffbb005b5e59c4032d7a8b8a71b226f21b6
mad_haosist <Legion-fire@yandex.ru>
2020-11-04 20:49:50 +0300

    Из главного класса убрал всё, что относилось к романской системе счисления

--- src/Main.java
+++ src/Main.java
@@ -3,100 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static boolean roman_flag;
     static int result;
-    static String result_roman;
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String str = reader.readLine();
-        String[] number_and_operation  = str.split(" ");
-        switch (number_and_operation[0]) {
-            case "I" -> {
-                number_and_operation[0] = String.valueOf(1);
-                roman_flag = true;
-            }
-            case "II" -> {
-                number_and_operation[0] = String.valueOf(2);
-                roman_flag = true;
-            }
-            case "III" -> {
-                number_and_operation[0] = String.valueOf(3);
-                roman_flag = true;
-            }
-            case "IV" -> {
-                number_and_operation[0] = String.valueOf(4);
-                roman_flag = true;
-            }
-            case "V" -> {
-                number_and_operation[0] = String.valueOf(5);
-                roman_flag = true;
-            }
-            case "VI" -> {
-                number_and_operation[0] = String.valueOf(6);
-                roman_flag = true;
-            }
-            case "VII" -> {
-                number_and_operation[0] = String.valueOf(7);
-                roman_flag = true;
-            }
-            case "VIII" -> {
-                number_and_operation[0] = String.valueOf(8);
-                roman_flag = true;
-            }
-            case "IX" -> {
-                number_and_operation[0] = String.valueOf(9);
-                roman_flag = true;
-            }
-            case "X" -> {
-                number_and_operation[0] = String.valueOf(10);
-                roman_flag = true;
-            }
-        }
-        switch (number_and_operation[2]) {
-            case "I" -> {
-                number_and_operation[2] = String.valueOf(1);
-                roman_flag = true;
-            }
-            case "II" -> {
-                number_and_operation[2] = String.valueOf(2);
-                roman_flag = true;
-            }
-            case "III" -> {
-                number_and_operation[2] = String.valueOf(3);
-                roman_flag = true;
-            }
-            case "IV" -> {
-                number_and_operation[2] = String.valueOf(4);
-                roman_flag = true;
-            }
-            case "V" -> {
-                number_and_operation[2] = String.valueOf(5);
-                roman_flag = true;
-            }
-            case "VI" -> {
-                number_and_operation[2] = String.valueOf(6);
-                roman_flag = true;
-            }
-            case "VII" -> {
-                number_and_operation[2] = String.valueOf(7);
-                roman_flag = true;
-            }
-            case "VIII" -> {
-                number_and_operation[2] = String.valueOf(8);
-                roman_flag = true;
-            }
-            case "IX" -> {
-                number_and_operation[2] = String.valueOf(9);
-                roman_flag = true;
-            }
-            case "X" -> {
-                number_and_operation[2] = String.valueOf(10);
-                roman_flag = true;
-            }
-        }
-        int number1 = Integer.parseInt(number_and_operation[0]);
-        int number2 = Integer.parseInt(number_and_operation[2]);
-        String arithmetic_operation = number_and_operation[1];
+        Roman.setNumber_and_operation(str);
+        boolean roman_flag = Roman.roman_flag;
+        int number1 = Roman.number1;
+        int number2 = Roman.number2;
+        String arithmetic_operation = Roman.arithmetic_operation;
 
         if (number1 > 10 || number2 > 10) throw new ArithmeticException("Одно из введенных чисел, или оба числа больше 10");
        if (number1 < 0 || number2 < 0) throw new ArithmeticException("Одно из введенных чисел, или оба числа меньше единицы");
@@ -107,7 +22,7 @@ public class Main {
             case "*" -> number1 * number2;
             default -> throw new ArithmeticException("Введена неверная операция");
         };
-         if (!roman_flag) System.out.println(result);
+           if (!roman_flag) System.out.println(result);
      else Roman.roman_numerals(result); // метод романус
     }
 }
--- src/Roman.java
+++ src/Roman.java
@@ -1,85 +1,103 @@
 public class Roman {
-    static boolean roman_flag = Main.roman_flag;
-    static int result = Main.result;
-    static String result_roman = Main.result_roman;
-   // static int number1;
-   // static int number2;
-   // static String arithmetic_operation;
 
-   // public static String setNumber_and_operation(String [] arg ) {
-       // String[] arg  = str.split(" ");
-//        switch (arg[0]) {
-//            case "I" -> {
-//                return arg[0] = String.valueOf(1);
-//            }
-//            case "II" -> {
-//                return arg[0] = String.valueOf(2);
-//            }
-//            case "III" -> {
-//                return arg[0] = String.valueOf(3);
-//            }
-//            case "IV" -> {
-//                return arg[0] = String.valueOf(4);
-//            }
-//            case "V" -> {
-//                return arg[0] = String.valueOf(5);
-//            }
-//            case "VI" -> {
-//                return arg[0] = String.valueOf(6);
-//            }
-//            case "VII" -> {
-//                return arg[0] = String.valueOf(7);
-//            }
-//            case "VIII" -> {
-//                return arg[0] = String.valueOf(8);
-//            }
-//            case "IX" -> {
-//                return arg[0] = String.valueOf(9);
-//            }
-//            case "X" -> {
-//                return arg[0] = String.valueOf(10);
-//            }
-//        }
-//        switch (arg[2]) {
-//            case "I" -> {
-//                return arg[2] = String.valueOf(1);
-//            }
-//            case "II" -> {
-//                return arg[2] = String.valueOf(2);
-//            }
-//            case "III" -> {
-//                return arg[2] = String.valueOf(3);
-//            }
-//            case "IV" -> {
-//                return arg[2] = String.valueOf(4);
-//            }
-//            case "V" -> {
-//                return arg[2] = String.valueOf(5);
-//            }
-//            case "VI" -> {
-//                return arg[2] = String.valueOf(6);
-//            }
-//            case "VII" -> {
-//                return arg[2] = String.valueOf(7);
-//            }
-//            case "VIII" -> {
-//                return arg[2] = String.valueOf(8);
-//            }
-//            case "IX" -> {
-//                return arg[2] = String.valueOf(9);
-//            }
-//            case "X" -> {
-//                return arg[2] = String.valueOf(10);
-//            }
-//        }
-//           number1 = Integer.parseInt(arg[0]);
-//           number2 = Integer.parseInt(arg[2]);
-//           arithmetic_operation = arg[1];
-//        System.out.println(number1);
-//        System.out.println(number2);
-//        System.out.println(arithmetic_operation);
-//        return null;
-//    }
+    static boolean roman_flag;
+    static int result;
+    static String result_roman;
+    static int number1;
+    static int number2;
+    static String arithmetic_operation;
+
+   public static String [] setNumber_and_operation(String  str ) {
+       String[] number_and_operation = str.split(" ");
+       switch (number_and_operation[0]) {
+           case "I" -> {
+               number_and_operation[0] = String.valueOf(1);
+               roman_flag = true;
+           }
+           case "II" -> {
+               number_and_operation[0] = String.valueOf(2);
+               roman_flag = true;
+           }
+           case "III" -> {
+               number_and_operation[0] = String.valueOf(3);
+               roman_flag = true;
+           }
+           case "IV" -> {
+               number_and_operation[0] = String.valueOf(4);
+               roman_flag = true;
+           }
+           case "V" -> {
+               number_and_operation[0] = String.valueOf(5);
+               roman_flag = true;
+           }
+           case "VI" -> {
+               number_and_operation[0] = String.valueOf(6);
+               roman_flag = true;
+           }
+           case "VII" -> {
+               number_and_operation[0] = String.valueOf(7);
+               roman_flag = true;
+           }
+           case "VIII" -> {
+               number_and_operation[0] = String.valueOf(8);
+               roman_flag = true;
+           }
+           case "IX" -> {
+               number_and_operation[0] = String.valueOf(9);
+               roman_flag = true;
+           }
+           case "X" -> {
+               number_and_operation[0] = String.valueOf(10);
+               roman_flag = true;
+           }
+       }
+       switch (number_and_operation[2]) {
+           case "I" -> {
+               number_and_operation[2] = String.valueOf(1);
+               roman_flag = true;
+           }
+           case "II" -> {
+               number_and_operation[2] = String.valueOf(2);
+               roman_flag = true;
+           }
+           case "III" -> {
+               number_and_operation[2] = String.valueOf(3);
+               roman_flag = true;
+           }
+           case "IV" -> {
+               number_and_operation[2] = String.valueOf(4);
+               roman_flag = true;
+           }
+           case "V" -> {
+               number_and_operation[2] = String.valueOf(5);
+               roman_flag = true;
+           }
+           case "VI" -> {
+               number_and_operation[2] = String.valueOf(6);
+               roman_flag = true;
+           }
+           case "VII" -> {
+               number_and_operation[2] = String.valueOf(7);
+               roman_flag = true;
+           }
+           case "VIII" -> {
+               number_and_operation[2] = String.valueOf(8);
+               roman_flag = true;
+           }
+           case "IX" -> {
+               number_and_operation[2] = String.valueOf(9);
+               roman_flag = true;
+           }
+           case "X" -> {
+               number_and_operation[2] = String.valueOf(10);
+               roman_flag = true;
+           }
+       }
+        number1 = Integer.parseInt(number_and_operation[0]);
+        number2 = Integer.parseInt(number_and_operation[2]);
+        arithmetic_operation = number_and_operation[1];
+       return number_and_operation;
+   }
 
     public static String roman_ten (int a) {
         if (roman_flag) {
@@ -123,6 +141,7 @@ public class Roman {
     }
 
     static void roman_numerals (int a) {
+       result = Main.result;
         if (roman_flag) {
             if (result <= 10) {
                 roman_ten(result);
